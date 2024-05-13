73028e5e3a11c2aecdef7a14a9bd8d7fc6112df4
Hennnesssy <ruslanshatkun@gmail.com>
2024-05-13 17:29:57 +0200

    Add project files.

---
+++ WindowsInformationFinder/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsInformationFinder
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.Timer timer;
+
+        //Mail Class
+        //string mailSubject = $"Statistki {DateTime.Now}";
+        //string mailBody = File.ReadAllText(@"C:\Users\Administrator\Desktop\statistic.txt");
+        string mailSubject;
+        string mailBody;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\", "*.*");
+            watcher.EnableRaisingEvents = true;
+            watcher.SynchronizingObject = this;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Created += new FileSystemEventHandler(Watcher_Created);
+            watcher.Deleted += new FileSystemEventHandler(Watcher_Deleted);
+            watcher.Renamed += new RenamedEventHandler(Watcher_Renamed);
+            
+            StartTimer();
+            //SendToMail(mailSubject, mailBody);
+        }
+        void Watcher_Created(object sender, FileSystemEventArgs e) {
+            string path = @"C:\Users\Administrator\Desktop\statistic.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path, true))
+                streamWriter.WriteLine($"file created \"{e.Name}\", {DateTime.Now}\n");
+            updateSendStatistic();
+        }
+        void Watcher_Renamed(object sender, FileSystemEventArgs e) {
+            string path = @"C:\Users\Administrator\Desktop\statistic.txt";
+            string nameFile = e.Name;
+            using (StreamWriter streamWriter = new StreamWriter(path, true))
+                streamWriter.WriteLine($"file \"{nameFile}\" renamed \"{e.Name}\", {DateTime.Now}\n");
+            updateSendStatistic();
+        }
+        void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string path = @"C:\Users\Administrator\Desktop\statistic.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path, true))
+                streamWriter.WriteLine($"file Deleted \"{e.Name}\", {DateTime.Now}\n");
+            updateSendStatistic();
+        }
+
+        void SendToMail(string mailSubject, string mailBody) 
+        {
+            Mail mail = new Mail();
+            mail.SendEmailNotification(mailSubject, mailBody);
+        }
+        void updateSendStatistic()
+        {
+            mailSubject = $"Statistki {DateTime.Now}";
+            mailBody = File.ReadAllText(@"C:\Users\Administrator\Desktop\statistic.txt");
+        }
+        void StartTimer()
+        {
+            timer = new System.Threading.Timer((e) =>
+            {
+                updateSendStatistic();
+                SendToMail(mailSubject, mailBody);
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));
+        }
+    }
+}
+
+    
---
+++ WindowsInformationFinder/Mail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+namespace WindowsInformationFinder
+{
+    class Mail
+    {
+        public void SendEmailNotification(string subject, string body)
+        {
+            var fromAddress = new MailAddress("oreshkayes@gmail.com", "fromRuslan");
+            var toAddress = new MailAddress("ruslanshatkun@gmail.com", "toRuslan");
+
+            const string fromPassword = "lfqs kmam lztt edbi";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            };
+        }
+    }
+}
